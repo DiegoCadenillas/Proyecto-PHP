5ad03fbcf76e247e9973a2b860c66e046fb0004b
JaviVL89 <bratenahl22@gmail.com>
2024-01-17 12:40:04 +0100

    terminado el archivo

--- controller/EliminarJuegoController.php
+++ controller/EliminarJuegoController.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace controller;
+namespace JUEGOSMESA\controller;
 
-use model\Utils as ModelUtils;
+use JUEGOSMESA\model\Juego as ModelJuego;
+use JUEGOSMESA\model\Utils as ModelUtils;
 
 // Incluir el archivo juego.php
 include('..\model\juego.php');
@@ -19,32 +20,21 @@ if (isset($_SESSION['user'])) {
     // Verificar la conexión exitosa antes de proceder
     if ($pdo) {
         // Validar y procesar el ID del juego a eliminar
-        $idJuego = ModelUtils::validarDatos($_POST['idJuego']);
+        $idJuego = ModelUtils::validar_datos($_POST['idJuego']);
 
         // Verificar si el ID es válido antes de proceder
         if (is_numeric($idJuego)) {
             // Eliminar el juego y verificar el resultado
-            $eliminacionExitosa = \model\juego::eliminarJuego($pdo, $idJuego);
+            $eliminacionExitosa = ModelJuego::del_juego($pdo, $idJuego);
 
-            if ($eliminacionExitosa) {
-                // Redirigir a la página principal de juegos
-                header('Location: ../view/MostrarJuegos.php');
-                exit();
-            } else {
-                echo "Error al eliminar el juego.";
-                // Puedes redirigir o mostrar un mensaje de error adecuado
-            }
-        } else {
-            echo "ID del juego no válido.";
-            // Puedes redirigir o mostrar un mensaje de error adecuado
-        }
-    } else {
-        echo "Error al conectar a la base de datos.";
-        // Puedes redirigir o mostrar un mensaje de error adecuado
-    }
-} else {
-    // Redirigir a la página de inicio de sesión si no hay una sesión iniciada
-    header('Location: ../view/Login.php');
-    exit();
+         //Cargamos la vista principal
+  //Cargamos los datos de los productos
+  $datosProducto = ModelJuego::get_juegos($pdo);
+
+  //Cargamos la vista
+  include('..\view/Mostrar_juegos.php');
+
+}
+}
 }
 ?>

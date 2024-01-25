351d83df51dda6bf22b555429ed82ef0eb9a5b98
batista <abatsan453@g.educaand.es>
2024-01-25 23:05:05 +0100

    vista inicia sesion en proceso

--- view/login.php
+++ view/login.php
@@ -1,2 +1,23 @@
-<?php
-echo "LOGIN";
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciar Sesión</title>
+</head>
+
+<body>
+    <h2>Iniciar Sesión</h2>
+    <form method="post" action="/controller/IniciarSesion.php">
+        <label for="correo">Correo Electrónico:</label>
+        <input type="email" name="correo" required><br>
+
+        <label for="contrasena">Contraseña:</label>
+        <input type="password" name="contrasena" required><br>
+
+        <input type="submit" value="Iniciar Sesión">
+    </form>
+</body>
+
+</html>

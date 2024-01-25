f6689deb04a7d045478cdcb9a38d4333727cf82f
JaviVL89 <bratenahl22@gmail.com>
2024-01-25 20:47:07 +0100

    archivo de confirmacion

---
+++ view/exito.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro Exitoso</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Registro Exitoso</h1>
+        <p>¡Gracias por registrarte! Hemos enviado un correo electrónico a tu dirección proporcionada con un enlace para activar tu cuenta. Por favor, revisa tu correo electrónico y sigue las instrucciones para completar el proceso de activación.</p>
+        <p><a href="Mostrar_juegos.php">Volver a la página principal</a></p>
+    </div>
+</body>
+</html>

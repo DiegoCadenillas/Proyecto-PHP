52c44c3031ac66c0a89b677554d72a0a32333798
batista28 <abatsan453@g.educaand.es>
2024-01-26 14:47:01 +0100

    añadido estilo inicia sesion

--- view/login.php
+++ view/login.php
@@ -5,8 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicia Sesión</title>
+    <link rel="stylesheet" href="login.css">
 
 </head>
+<header>
+    <h1>Tablegames</h1>
+</header>
 
 <body>
     <h2>Iniciar Sesión</h2>
@@ -22,5 +26,8 @@
 
     <p>¿No tienes una cuenta? <a href="Registro.html">Regístrate aquí</a>.</p>
 </body>
+<footer>
+
+</footer>
 
 </html>

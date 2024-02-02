1634cafc0e7caaea28c11c88207405a2ca4dba94
JaviVL89 <bratenahl22@gmail.com>
2024-02-02 12:48:07 +0100

    cerrar sesion controlador

--- controller/CerrarSesionController.php
+++ controller/CerrarSesionController.php
@@ -1,24 +1,11 @@
 <?php
-namespace JUEGOSMESA\controller;
-
 // Iniciar la sesión solo una vez al principio del script
 if (session_status() != PHP_SESSION_ACTIVE) session_start();
+// Destruir la sesión
+session_unset();
+session_destroy();
 
-// Verificar si el usuario está logado
-if (isset($_SESSION['user'])) {
-    // Verificar el tiempo de inactividad
-    $inactividad = 300; // 5 minutos en segundos
-
-    if (isset($_SESSION['tiempo']) && (time() - $_SESSION['tiempo'] > $inactividad)) {
-        // Si ha pasado más de 5 minutos de inactividad, destruir la sesión
-        session_unset();
-        session_destroy();
-        include('../view/index.php'); // Redirigir al usuario a la página de inicio de sesión
-        exit();
-    } else {
-        // Si el usuario está activo, actualiza el tiempo de actividad
-        $_SESSION['tiempo'] = time();
-    }
-} 
-
+// Redirigir al usuario a la página de inicio de sesión u otra página según tus necesidades
+include("Location: ../view/index.php");
+exit();
 ?>

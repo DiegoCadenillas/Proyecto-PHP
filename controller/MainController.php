c55a229a7525e5c53980995370e44b5fcfc46859
DiegoCadenillas <dabonkinator845@gmail.com>
2024-02-03 08:17:33 +0100

    Deprecada funcionalidad de cierre de sesión en MainController.php.

--- controller/MainController.php
+++ controller/MainController.php
@@ -20,19 +20,6 @@ if (isset($_SESSION["user"])) {
         // Cargar los datos de los juegos
         $datos_juegos = ModelJuego::get_juegos($pdo);
     }
-    // Verificar el tiempo de inactividad
-    $inactividad = 300; // 5 minutos en segundos
-
-    if (isset($_SESSION['tiempo']) && (time() - $_SESSION['tiempo'] > $inactividad)) {
-        // Si ha pasado más de 5 minutos de inactividad, destruir la sesión
-        session_unset();
-        session_destroy();
-        include('../view/index.php'); // Redirigir al usuario a la página de inicio de sesión
-        exit();
-    } else {
-        // Si el usuario está activo, actualiza el tiempo de actividad
-        $_SESSION['tiempo'] = time();
-    }
 
     include("../view/Mostrar_juegos.php");
 } else {
